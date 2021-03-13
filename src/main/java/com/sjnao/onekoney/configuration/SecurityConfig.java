0e5713cacdd1feecf02af450fd860357b89a33f9
Jonas Nicoletti <sjnao89@gmail.com>
2021-03-13 17:02:21 +0100

    basic security

---
+++ src/main/java/com/sjnao/onekoney/configuration/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.sjnao.onekoney.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                "/configuration/security", "/swagger-ui.html", "/webjars/**");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        // .and() more rules
+    }
+}
